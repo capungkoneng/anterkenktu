dcde5b75557205ded87e425c12deecf96f3e6570
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-07-12 23:30:33 +0700

    deploy 1

--- api/server.go
+++ api/server.go
@@ -1,15 +1,28 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/capungkoneng/anterkenktu/db/sqlc"
 	"github.com/gin-gonic/gin"
 )
 
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 func NewServer(store *db.Store) *Server {
-	r := gin.Default()
+	server := &Server{store: *store}
+	fmt.Println(server)
+	router := gin.Default()
+
+	router.GET("/", server.GetListKategori)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
 }
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"database/sql"
 	"log"
+
+	"github.com/capungkoneng/anterkenktu/api"
+	db "github.com/capungkoneng/anterkenktu/db/sqlc"
 )
 
 func main() {
@@ -11,7 +14,12 @@ func main() {
 		log.Fatal("cannot connect to database", err)
 	}
 	store := db.NewStore(conn)
-	server := api.NewServer(store)
+	server := api.NewServer(&store)
+
+	err = server.Start("")
+	if err != nil {
+		log.Fatal("cannot connect to server", err)
+	}
 
 	// server.SetTrustedProxies(nil)
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
