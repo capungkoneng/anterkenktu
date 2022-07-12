0227de124a828d57232a77ad5cd8daea14a0e667
angga pcaw <anggapcaw@anggas-MacBook-Air.local>
2022-07-12 19:58:51 +0700

    deploy 1

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.SetTrustedProxies(nil)
+
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	server.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
